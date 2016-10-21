d82ed0589bd026073f5b80fa44b30db93cecc878
Alexis Lameire <alexis.lameire@gmail.com>
2016-10-21 20:11:39 +0200

    add recursion for parameter set

--- ratftpd/config.py
+++ ratftpd/config.py
@@ -26,28 +26,34 @@ class Config(object):
         self.networks = network
         for network in networks[1:]:
             self.networks.addChild(network)
-        self.networks.makeTree()
+        self.networks.makeTree(self)
         
 
 class NetworkConfig(object):
     ''' manage a configuration for a network '''
 
     def __init__(self, parent, config):
-        self.timeout = config.get('timeout', parent.timeout)
-        self.retry = config.get('retry', parent.retry)
         if 'network' not in config:
             raise ValueError('network argument is required for a network entry')
         self.network = ip_network(config['network'])
         self.subnets = []
+        self._config = config
+
+    def _setConfig(self):
+        self.timeout = self._config.get('timeout', self.parent.timeout)
+        self.retry = self._config.get('retry', self.parent.retry)
+        del self._config
 
     def addChild(self, child):
         self.subnets.append(child)
 
-    def makeTree(self):
+    def makeTree(self, parent):
         refIndex = 0
         currentIndex = 1
         networks = self.subnets
         self.subnets = []
+        self.parent = parent
+        self._setConfig()
         for i in range(1, len(networks)):
             if networks[refIndex].network.overlaps(
                  networks[currentIndex].network
@@ -56,7 +62,7 @@ class NetworkConfig(object):
                 currentIndex += 1
             else:
                 self.subnets.append(networks[refIndex])
-                networks[refIndex].makeTree()
+                networks[refIndex].makeTree(self)
                 refIndex = currentIndex
                 currentIndex += 1
             if currentIndex >= len(networks) or refIndex >= len(networks):
@@ -64,8 +70,7 @@ class NetworkConfig(object):
 
         if refIndex < len(networks):
            self.subnets.append(networks[refIndex])
-           networks[refIndex].makeTree()
-        
+           networks[refIndex].makeTree(self)
 
     def _getNumeric(self, other):
         cmpVal = self.network.compare_networks(other.network)
